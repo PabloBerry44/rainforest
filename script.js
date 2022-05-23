599874f325624a01d3c3d67be5c707d74eaa326b
pabloberry44 <pawel.borowiec@tutanota.com>
2022-05-23 12:23:28 +0200

    hero section mobile and desktop ready

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const tucan1 = document.querySelector('#tucan1')
-const tucan2 = document.querySelector('#tucan2')
 const right_tree = document.querySelector('#right_tree')
 const left_fern = document.querySelector('#left_fern')
 
@@ -9,7 +8,6 @@ window.addEventListener('scroll', ()=>{
     let rotation = (-windowPosition*0.09)+40 + 'deg'
 
     tucan1.style.transform = 'translateY('+distance+')'
-    tucan2.style.transform = 'rotateZ('+rotation+')'
     right_tree.style.transform = 'translateX('+windowPosition*0.01 + 'em'+') rotateZ(-20deg)'
     left_fern.style.transform = 'translateX('+windowPosition*(-0.01) + 'em'+')'
 
