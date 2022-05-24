50d9432ab31d852f193bd5bee4d95c6c2cac909b
pabloberry44 <pawel.borowiec@tutanota.com>
2022-05-24 13:21:50 +0200

    parallax effect

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    // true for mobile device
-    console.log('mobile')
-  }else{
-    const tucan1 = document.querySelector('#tucan1')
-    const right_tree = document.querySelector('#right_tree')
-    const left_fern = document.querySelector('#left_fern')
-    
-    window.addEventListener('scroll', ()=>{
-        const windowPosition = window.scrollY
-        let distance = windowPosition*0.04 + 'em'
-        let rotation = (-windowPosition*0.09)+40 + 'deg'
-    
-        tucan1.style.transform = 'translateY('+distance+')'
-        right_tree.style.transform = 'translateX('+windowPosition*0.01 + 'em'+') rotateZ(-20deg)'
-        left_fern.style.transform = 'translateX('+windowPosition*(-0.01) + 'em'+')'
-    })
-  }
+function Animation()
+{
+  const tucan1 = document.querySelector('#tucan1')
+  const right_tree = document.querySelector('#right_tree')
+  const left_fern = document.querySelector('#left_fern')
+  const photo = document.querySelector('#photo')
+  const parallax = document.querySelector('.parallax')
+  
+  window.addEventListener('scroll', ()=>{
+      const positionY = window.scrollY
+      let distance = positionY*0.04 + 'em'
+      let rotation = (-positionY*0.09)+40 + 'deg'
+  
+      tucan1.style.transform = 'translateY('+distance+')'
+      right_tree.style.transform = 'translateX(' +positionY*0.01+ 'em) rotateZ(-20deg)'
+      left_fern.style.transform = 'translateX(' +positionY*(-0.01)+ 'em)'
+
+      let heig = photo.clientHeight/3
+      photo.style.bottom = heig+'px'
+      photo.style.transform = 'translateY('+positionY*0.1+'px)'
+
+
+  })
+
+
+
+
+}
+
+
+
+
+
+
+
+
+if(!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)))
+{
+  Animation()
+}
